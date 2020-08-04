003addf1cd0ee8db240629b07d22902eb6a536f7
cgraves09 <christopher.graves09@gmail.com>
2020-08-03 20:49:32 -0400

    Updates

---
+++ precache-manifest.92ece18e5d89c166ae223a4f9fc16d22.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0eb89773463d0c69aaca5c3fd7f6614",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2fd2e39446c96733a4d6",
+    "url": "/Portfolio/static/css/2.25249b3f.chunk.css"
+  },
+  {
+    "revision": "9a564a40045115069c6d",
+    "url": "/Portfolio/static/css/main.384dd7c0.chunk.css"
+  },
+  {
+    "revision": "2fd2e39446c96733a4d6",
+    "url": "/Portfolio/static/js/2.dc233fb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.dc233fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a564a40045115069c6d",
+    "url": "/Portfolio/static/js/main.f0dcfe23.chunk.js"
+  },
+  {
+    "revision": "7e7959629d8fd73d96a3",
+    "url": "/Portfolio/static/js/runtime-main.e3c0881a.js"
+  },
+  {
+    "revision": "e5411a5770a5e806cd1d03d6a976941c",
+    "url": "/Portfolio/static/media/Self.e5411a57.jpg"
+  },
+  {
+    "revision": "e30c1043dfdfa6a983ef8bbd8e5f0227",
+    "url": "/Portfolio/static/media/brewivery.e30c1043.gif"
+  },
+  {
+    "revision": "b76f9bc65466c5de398953da37d6de6e",
+    "url": "/Portfolio/static/media/food.b76f9bc6.gif"
+  },
+  {
+    "revision": "90759ab28133da79397ecae247160b13",
+    "url": "/Portfolio/static/media/html.90759ab2.gif"
+  },
+  {
+    "revision": "54da9bab6edefc998d1f1930df47afd6",
+    "url": "/Portfolio/static/media/marker.54da9bab.gif"
+  },
+  {
+    "revision": "9703accc3482354d2871d4edb199d011",
+    "url": "/Portfolio/static/media/robot.9703accc.gif"
+  },
+  {
+    "revision": "613102c8ca4d6b8773bc72ab89e60103",
+    "url": "/Portfolio/static/media/server.613102c8.gif"
+  },
+  {
+    "revision": "3d15c02c365af251d1ad40ea291d34ff",
+    "url": "/Portfolio/static/media/tools.3d15c02c.gif"
+  }
+]);
