7ee96ada0782b2fb2a5a6f9dbbeb1911637d0530
Christopher Graves <christopher.graves@a-lign.com>
2021-05-11 20:48:32 -0400

    Updates

---
+++ precache-manifest.1f364bebefc556d2a6a6f5b2dd92dbb0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9e7ee5069bd49f222a1440778145a6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7e8b8dbf37cd76b1ef13",
+    "url": "/Portfolio/static/css/2.25249b3f.chunk.css"
+  },
+  {
+    "revision": "880480c56cebfcaa184a",
+    "url": "/Portfolio/static/css/main.e16aea7b.chunk.css"
+  },
+  {
+    "revision": "7e8b8dbf37cd76b1ef13",
+    "url": "/Portfolio/static/js/2.666c3d3a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.666c3d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880480c56cebfcaa184a",
+    "url": "/Portfolio/static/js/main.92c4eae5.chunk.js"
+  },
+  {
+    "revision": "7e7959629d8fd73d96a3",
+    "url": "/Portfolio/static/js/runtime-main.e3c0881a.js"
+  },
+  {
+    "revision": "e5411a5770a5e806cd1d03d6a976941c",
+    "url": "/Portfolio/static/media/Self.e5411a57.jpg"
+  },
+  {
+    "revision": "e30c1043dfdfa6a983ef8bbd8e5f0227",
+    "url": "/Portfolio/static/media/brewivery.e30c1043.gif"
+  },
+  {
+    "revision": "b76f9bc65466c5de398953da37d6de6e",
+    "url": "/Portfolio/static/media/food.b76f9bc6.gif"
+  },
+  {
+    "revision": "90759ab28133da79397ecae247160b13",
+    "url": "/Portfolio/static/media/html.90759ab2.gif"
+  },
+  {
+    "revision": "54da9bab6edefc998d1f1930df47afd6",
+    "url": "/Portfolio/static/media/marker.54da9bab.gif"
+  },
+  {
+    "revision": "9703accc3482354d2871d4edb199d011",
+    "url": "/Portfolio/static/media/robot.9703accc.gif"
+  },
+  {
+    "revision": "613102c8ca4d6b8773bc72ab89e60103",
+    "url": "/Portfolio/static/media/server.613102c8.gif"
+  },
+  {
+    "revision": "3d15c02c365af251d1ad40ea291d34ff",
+    "url": "/Portfolio/static/media/tools.3d15c02c.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d09998d4a2a01b2b80157268b9e364d5.js"
+  "/Portfolio/precache-manifest.1f364bebefc556d2a6a6f5b2dd92dbb0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
